a23f9afb9905b0b3fd9f376768333cc53f652a49
Polichinell <138169337+Polichinell@users.noreply.github.com>
2023-08-30 16:28:37 +0200

    done

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+ const rword = word.toLowerCase();
+  const reverse = rword.split('').reverse().join('');
+  return rword === reverse;
 }
 
 /* 
-  Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
 */
 
 /*
-  Add written explanation of your solution here
+ The algorithm checks whether a word is a palindrome. It compares characters from both ends towards the middle. 
+ If all pairs match, it confirms a palindrome and returns true. A single mismatch proves it's not a palindrome, 
+ triggering a false result.
 */
 
 // You can run `node index.js` to view these console logs
@@ -19,7 +27,23 @@ if (require.main === module) {
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("Cersei"));
+
+   console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("level"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
 }
 
 module.exports = isPalindrome;
